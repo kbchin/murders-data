fe3d8cb40795c550487664784be576b9c4792bb3
kbchin <53152472+kbchin@users.noreply.github.com>
2019-07-28 20:16:14 -0400

    Delete wrangle-data.R

--- murders/rda/wrangle-data.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-murders <- read_csv("murders/data.csv")
-murders <- murders %>% mutate(region = factor(region), rate = total/population * 10^5)
-save(murders, file = "murders/rda.rda")
-
-
